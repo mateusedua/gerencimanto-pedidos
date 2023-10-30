54e2ce1a8e06dee5f1b2035201e868e2a3ba5f0f
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-30 23:50:30 +0000

    validate user and return user found

---
+++ src/app/api/service/userService.js
@@ -0,0 +1,30 @@
+import userData from "../data/userData"
+import validData from "@/app/utils/validData"
+import { encodeData } from "../lib/jwt"
+
+const getUserService = async (data) => {
+
+    if (validData(data.email)) return new Response("email not found", { status: 404 })
+    if (validData(data.password)) return new Response("password not found", { status: 404 })
+
+    const result = await userData.getUserData(data)
+
+    if (result.length > 0) {
+        const token = encodeData(result[0])
+
+        return new Response(
+            JSON.stringify({
+                token: token,
+                data: result[0]
+            })
+        )
+    }
+
+    return new Response("user not found", { status: 404 })
+
+}
+
+
+export default {
+    getUserService
+}
