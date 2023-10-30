05313eb758fd96019f9c91bae065e45567895118
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-30 23:52:15 +0000

    resolving errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,16 @@
+'use client'
 
 import { Providers } from "@/providers"
+import { UserProvider } from "./Context/UserProvider"
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
         <Providers>
-          {children}
+          <UserProvider>
+            {children}
+          </UserProvider>
         </Providers>
       </body>
     </html>
