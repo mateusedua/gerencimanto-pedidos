0d7a586c8396524d8f75a3273ddbbe2aedc89fc3
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-30 23:51:20 +0000

    return user login

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,11 +1,11 @@
-
+import userService from "../service/userService"
 
 
 
 export async function POST(request) {
     const body = await request.json()
 
+    const result = await userService.getUserService(body)
 
-
-    return Response.json({})
+    return result
 }
