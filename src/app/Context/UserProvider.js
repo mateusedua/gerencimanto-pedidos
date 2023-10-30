62680f2a3b846b2e97ae0973e1292554e7315045
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-30 23:48:46 +0000

    add function login for context

--- src/app/Context/UserProvider.js
+++ src/app/Context/UserProvider.js
@@ -1,18 +1,21 @@
-import { createContext, useContext } from "react";
 
+import { createContext, useContext } from "react";
+import request from "../utils/request";
 
 export const userContext = createContext({})
 
 
 export const UserProvider = ({ children }) => {
 
-    const login = (data) => {
+    const login = async (data) => {
+        const result = await request('/api/user', 'POST', data)
 
+        return result.status
     }
 
     return (
         <userContext.Provider value={{
-
+            login
         }}>
             {children}
         </userContext.Provider>
