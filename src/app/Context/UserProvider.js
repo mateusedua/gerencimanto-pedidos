7399308770944adc63b0cf1d53ac8d3911cb8567
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-31 01:42:52 +0000

    add set cookies on function login context

--- src/app/Context/UserProvider.js
+++ src/app/Context/UserProvider.js
@@ -1,21 +1,36 @@
+'use client'
 
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import request from "../utils/request";
+import { setCookies } from "./cookies";
 
 export const userContext = createContext({})
 
 
 export const UserProvider = ({ children }) => {
 
+    const [isLoading, setIsLoading] = useState(false)
+
     const login = async (data) => {
+        setIsLoading(true)
         const result = await request('/api/user', 'POST', data)
+        setIsLoading(false)
+
+        if (result.status === 200) {
+            const token = result.data.token
+            const nome = result.data.data.nome_usuario
+            const idUser = result.data.data.id_usuario
+
+            setCookies(token, nome, idUser)
+        }
 
         return result.status
     }
 
     return (
         <userContext.Provider value={{
-            login
+            login,
+            isLoading
         }}>
             {children}
         </userContext.Provider>
