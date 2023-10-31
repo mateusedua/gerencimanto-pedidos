6fbd8c32e21768d927521ea7bf9c2461b1110e38
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-10-31 01:40:52 +0000

    validate user found

--- src/app/(public)/layout.js
+++ src/app/(public)/layout.js
@@ -1,6 +1,11 @@
-
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
 export default function RootLayout({ children }) {
+
+    const { value } = cookies().get('token')
+    if (value) redirect('/')
+
     return (
         <>
             {children}
