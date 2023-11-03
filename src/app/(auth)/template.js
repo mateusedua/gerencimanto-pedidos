c8d17d9aef3f777eb68c87678bd37ae3ea57b7de
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-11-03 21:29:46 +0000

    add container

--- src/app/(auth)/template.js
+++ src/app/(auth)/template.js
@@ -1,15 +1,16 @@
 import Header from "./Header"
 import { getToken } from "../Context/cookies"
 import { redirect } from "next/navigation"
+import { Container } from "@chakra-ui/react"
 
 export default function Template({ children }) {
 
     if (getToken() === undefined) redirect('/login')
 
     return (
-        <>
+        <Container maxW={'100%'}>
             <Header />
             {children}
-        </>
+        </Container>
     )
 }
