1a2302a1514cdd07a2b6bfee612e1070d61081b1
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-11-03 22:49:00 +0000

    add tamplate separate and head

--- src/app/(auth)/template.js
+++ src/app/(auth)/template.js
@@ -1,16 +1,15 @@
+'use client'
+
 import Header from "./Header"
-import { getToken } from "../Context/cookies"
-import { redirect } from "next/navigation"
-import { Container } from "@chakra-ui/react"
 
 export default function Template({ children }) {
-
-    if (getToken() === undefined) redirect('/login')
-
     return (
-        <Container maxW={'100%'}>
+        <>
+            <head>
+                <title>Início</title>
+            </head>
             <Header />
             {children}
-        </Container>
+        </>
     )
 }
