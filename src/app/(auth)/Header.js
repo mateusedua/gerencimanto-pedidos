05f2ca7268afba88a7b95542035a69af9a56cbf1
Mateus Eduardo <mateuseduardoteuta10@gmail.com>
2023-11-03 21:16:39 +0000

    add perfil user

--- src/app/(auth)/Header.js
+++ src/app/(auth)/Header.js
@@ -1,9 +1,14 @@
 'use client'
 
-import { Flex, useColorModeValue, Box, Image, Text } from "@chakra-ui/react"
+import { Flex, useColorModeValue, Box, Image, Text, Menu, MenuButton, Avatar, MenuList, Center, MenuDivider, MenuItem } from "@chakra-ui/react"
 import Link from "next/link"
 
 const Header = () => {
+
+    const handleSair = () => {
+
+    }
+
     return (
         <Flex
             alignItems='center'
@@ -12,50 +17,82 @@ const Header = () => {
             borderBottom={1}
             borderStyle={'solid'}
             borderColor={useColorModeValue('gray.200', 'gray.900')}
+            position={'fixed'}
+            zIndex={999}
+            width={'100%'}
+            justifyContent={'space-between'}
         >
-            <Box marginRight='50px'>
-                <Link href='/'>
-                    <Image
-                        borderRadius='full'
-                        boxSize='50px'
-                        src="logo.png"
+            <Flex>
+                <Box marginRight='50px'>
+                    <Link href='/'>
+                        <Image
+                            borderRadius='full'
+                            boxSize='50px'
+                            src="logo.png"
+                            cursor='pointer'
+                        />
+                    </Link>
+                </Box>
+                <Link
+                    href="/"
+                >
+                    <Text fontSize='xl'
                         cursor='pointer'
-                    />
+                        fontWeight={560} p={2}
+                        color={useColorModeValue('gray.600', 'gray.200')}
+                        _hover={{
+                            color: useColorModeValue('gray.800', 'white'),
+                        }}>Início</Text>
                 </Link>
-            </Box>
-            <Link
-                href="/"
-            >
-                <Text fontSize='xl'
-                    cursor='pointer'
-                    fontWeight={560} p={2}
-                    color={useColorModeValue('gray.600', 'gray.200')}
-                    _hover={{
-                        color: useColorModeValue('gray.800', 'white'),
-                    }}>Início</Text>
-            </Link>
-            <Link
-                href="/Produto"
-            >
-                <Text fontSize='xl'
-                    cursor='pointer'
-                    fontWeight={560} p={2}
-                    color={useColorModeValue('gray.600', 'gray.200')}
-                    _hover={{
-                        color: useColorModeValue('gray.800', 'white'),
-                    }}>Produto</Text>
-            </Link>
-            <Link
-                href='/Cliente'
-            >
-                <Text fontSize='xl'
-                    cursor='pointer'
-                    fontWeight={560} p={2}
-                    color={useColorModeValue('gray.600', 'gray.200')}
-                    _hover={{
-                        color: useColorModeValue('gray.800', 'white'),
-                    }}>Cliente</Text>
-            </Link>
+                <Link
+                    href="/Produto"
+                >
+                    <Text fontSize='xl'
+                        cursor='pointer'
+                        fontWeight={560} p={2}
+                        color={useColorModeValue('gray.600', 'gray.200')}
+                        _hover={{
+                            color: useColorModeValue('gray.800', 'white'),
+                        }}>Produto</Text>
+                </Link>
+                <Link
+                    href='/Cliente'
+                >
+                    <Text fontSize='xl'
+                        cursor='pointer'
+                        fontWeight={560} p={2}
+                        color={useColorModeValue('gray.600', 'gray.200')}
+                        _hover={{
+                            color: useColorModeValue('gray.800', 'white'),
+                        }}>Cliente</Text>
+                </Link>
+            </Flex>
+            <Menu>
+                <MenuButton
+                    rounded={'full'}
+                    variant={'link'}
+                    cursor={'pointer'}
+                >
+                    <Avatar
+                        size={'md'}
+                    />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                    <br />
+                    <Center>
+                        <Avatar
+                            size={'lg'}
+                        />
+                    </Center>
+                    <br />
+                    <Center>
+                        <span>Mateus Eduardo</span>
+                    </Center>
+                    <br />
+                    <MenuDivider />
+                    <MenuItem onClick={handleSair}>Sair</MenuItem>
+                </MenuList>
+            </Menu>
         </Flex>
     )
 }
