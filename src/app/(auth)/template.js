20f07d16b3d405137961a18355e132e37b0a6bcc
Mateus Eduardo <mateuseduardobene@gmail.com>
2023-11-04 09:22:05 -0300

    add validate token

--- src/app/(auth)/template.js
+++ src/app/(auth)/template.js
@@ -1,8 +1,12 @@
-'use client'
 
 import Header from "./Header"
+import { getToken } from "@/Context/cookies"
+import { redirect } from "next/navigation"
 
 export default function Template({ children }) {
+
+    if (getToken() === undefined) redirect('/login')
+
     return (
         <>
             <Header />
