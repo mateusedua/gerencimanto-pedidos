4bd0dba8d1d3ae9c64a3cccbedad7b978effaea4
Mateus Eduardo <mateuseduardobene@gmail.com>
2023-11-16 16:34:13 -0300

    add function for product registration

--- src/Context/ProdutoProvider.js
+++ src/Context/ProdutoProvider.js
@@ -1,16 +1,24 @@
 'use client'
 
-import { createContext, useContext, useEffect } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import request from "@/utils/request";
 
 export const produtoContext = createContext({})
 
 export const ProdutoProvider = ({ children }) => {
 
+    const [produtos, setProdutos] = useState([])
+    const [selectProdutos, setSelectProdutos] = useState([])
 
     const handleProdutos = async () => {
         const result = await request('/api/produto', 'GET')
-        console.log(result.data)
+        setProdutos(result.data)
+        setSelectProdutos(result.data)
+    }
+
+    const handleCadastroProduto = async (data) => {
+        const result = await request('/api/produto/cadastroProduto', 'POST', data)
+        return result
     }
 
     useEffect(() => {
@@ -21,7 +29,10 @@ export const ProdutoProvider = ({ children }) => {
     }, [])
 
     return <produtoContext.Provider value={{
-
+        selectProdutos,
+        setSelectProdutos,
+        produtos,
+        handleCadastroProduto
     }}>
         {children}
     </produtoContext.Provider>
