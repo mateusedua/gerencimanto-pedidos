fa6cabf49200ad8dc47310dc4323f257d4a64a6e
Mateus Eduardo <mateuseduardobene@gmail.com>
2023-11-16 16:35:00 -0300

    add function for insert product

--- src/app/api/data/produtoData.js
+++ src/app/api/data/produtoData.js
@@ -10,7 +10,17 @@ const getProdutos = async () => {
     return result.rows
 }
 
+const insertProduto = async (data) => {
+    const conn = connect(config)
+    const result = await conn.execute(`
+        insert into produto(nome_produto,preco_produto,quantidade_produto,unidade)
+        values(?,?,?,?)
+    `, [data.nome, data.preco, data.quantidade, data.unidade])
+
+    return result.rowsAffected
+}
 
 export default {
-    getProdutos
+    getProdutos,
+    insertProduto
 }
