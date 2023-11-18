72fd6f0fbb73d6b47a7d479e25b7c45366e03742
Mateus Eduardo <mateuseduardobene@gmail.com>
2023-11-18 09:44:15 -0300

    add component alert

---
+++ src/components/alertToast.js
@@ -0,0 +1,22 @@
+import Swal from "sweetalert2";
+
+const alertToast = (icon, title) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 9000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    })
+
+    return Toast.fire({
+        icon: icon,
+        title: title
+    })
+}
+
+export default alertToast
