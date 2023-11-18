9ffa5878e429c0e04598853ce177a79472618082
Mateus Eduardo <mateuseduardobene@gmail.com>
2023-11-18 09:47:22 -0300

    validate if user does not exist and add kepress event enter

--- src/app/(public)/login/page.js
+++ src/app/(public)/login/page.js
@@ -17,6 +17,7 @@ import isEmail from "validator/lib/isEmail"
 import { useUserProvider } from "@/Context/UserProvider"
 import { MdVisibility } from 'react-icons/md'
 import { useState } from "react"
+import alertToast from "@/components/alertToast"
 
 const Login = () => {
 
@@ -27,86 +28,90 @@ const Login = () => {
     const router = useRouter()
 
 
-    const handleLogin = async (data) => {
+    const onSubmit = async (data) => {
         const status = await login(data)
 
         if (status === 200) {
             return router.push('/')
         }
 
+        alertToast('error', 'Usuário não encontrado')
+
     }
 
     return (
-        <Center minH={'100vh'}>
-            <Flex minW={400}
-                h={450}
-                direction='column'
-                alignItems='center'
-                justify='center'
-                borderWidth={isLargerThan300 ? 'none' : '1px'}
-                borderStyle='solid'
-                borderRadius='lg'
-                borderColor={useColorModeValue('gray.300', 'gray.900')}>
-                <Image src="logo.png"
-                    boxSize='120px'
-                    borderRadius='full'
-                />
-                <Flex direction={'column'} minW={350} gap={3}>
-                    <Flex direction='column' gap={2}>
-                        <label htmlFor="email">
-                            E-mail
-                        </label>
-                        <Input
-                            variant='filled'
-                            placeholder="E-mail ..."
-                            {...register('email', { required: true, validate: (value) => isEmail(value) })}
-                        />
-                        {
-                            errors?.email?.type === 'required' &&
-                            <span style={{
-                                color: 'red'
-                            }}>E-mail necessário</span>
-                        }
-                        {
-                            errors?.email?.type === 'validate' &&
-                            <span style={{
-                                color: 'red'
-                            }}>E-mail invalído</span>
-                        }
-                    </Flex>
-                    <Flex direction='column' gap={2}>
-                        <label htmlFor="password">
-                            Senha
-                        </label>
-                        <InputGroup>
+        <form onSubmit={handleSubmit(onSubmit)} onKeyDown={(e) => e.key === 'Enter' && handleSubmit(onSubmit)()}>
+            <Center minH={'100vh'}>
+                <Flex minW={400}
+                    h={450}
+                    direction='column'
+                    alignItems='center'
+                    justify='center'
+                    borderWidth={isLargerThan300 ? 'none' : '1px'}
+                    borderStyle='solid'
+                    borderRadius='lg'
+                    borderColor={useColorModeValue('gray.300', 'gray.900')}>
+                    <Image src="logo.png"
+                        boxSize='120px'
+                        borderRadius='full'
+                    />
+                    <Flex direction={'column'} minW={350} gap={3}>
+                        <Flex direction='column' gap={2}>
+                            <label htmlFor="email">
+                                E-mail
+                            </label>
                             <Input
                                 variant='filled'
-                                placeholder="*******"
-                                type={show ? 'text' : 'password'}
-                                {...register('password', { required: true })}
+                                placeholder="E-mail ..."
+                                {...register('email', { required: true, validate: (value) => isEmail(value) })}
                             />
-                            <InputRightElement width='4rem' height={'2.5rem'}>
-                                <MdVisibility
-                                    style={{
-                                        width: 25,
-                                        height: 25,
-                                        cursor: 'pointer'
-                                    }}
-                                    onClick={() => setShow(!show)}
+                            {
+                                errors?.email?.type === 'required' &&
+                                <span style={{
+                                    color: 'red'
+                                }}>E-mail necessário</span>
+                            }
+                            {
+                                errors?.email?.type === 'validate' &&
+                                <span style={{
+                                    color: 'red'
+                                }}>E-mail invalído</span>
+                            }
+                        </Flex>
+                        <Flex direction='column' gap={2}>
+                            <label htmlFor="password">
+                                Senha
+                            </label>
+                            <InputGroup>
+                                <Input
+                                    variant='filled'
+                                    placeholder="*******"
+                                    type={show ? 'text' : 'password'}
+                                    {...register('password', { required: true })}
                                 />
-                            </InputRightElement>
+                                <InputRightElement width='4rem' height={'2.5rem'}>
+                                    <MdVisibility
+                                        style={{
+                                            width: 25,
+                                            height: 25,
+                                            cursor: 'pointer'
+                                        }}
+                                        onClick={() => setShow(!show)}
+                                    />
+                                </InputRightElement>
+                            </InputGroup>
                             {
                                 errors?.password?.type === 'required' &&
                                 <span style={{
                                     color: 'red'
                                 }}>Senha necessária</span>
                             }
-                        </InputGroup>
+                        </Flex>
+                        <Button isLoading={isLoading} colorScheme='blue' type="submit">Entrar</Button>
                     </Flex>
-                    <Button isLoading={isLoading} colorScheme='blue' onClick={() => handleSubmit(handleLogin)()}>Entrar</Button>
                 </Flex>
-            </Flex>
-        </Center>
+            </Center >
+        </form>
     )
 }
 
